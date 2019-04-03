70ff98faf528fe682d8cfe5ad5f1d8f390d6d9c1
broskmenmi <brosk@menmi.se>
2019-04-03 21:57:53 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const expbs = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+
+app.engine('handlebars', expbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'views/mainLayout')
+}));
+
+app.set('view engine', 'handlebars');
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.listen(8080, () => {
+    console.info('Server started at port ', 8080);
+});
