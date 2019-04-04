bdbd08c8afb8b33dd381a06354cd05c66f7963d9
broskmenmi <brosk@menmi.se>
2019-04-04 21:35:38 +0200

    Calc page

--- server.js
+++ server.js
@@ -9,18 +9,28 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.engine('handlebars', expbs({
     defaultLayout: 'main',
-    layoutsDir: path.join(__dirname, 'views/mainLayout')
+    layoutsDir: path.join(__dirname, 'views/mainLayout'),
+    partialsDir: path.join(__dirname, '/views/partials/')
 }));
 
 app.set('view engine', 'handlebars');
 
 // Routes
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { showVideo: true });
 });
 
 app.get('/about', (req, res) => {
     res.render('about');
+});
+
+app.get('/calc', (req, res) => {
+
+    let age = req.query.age;
+    let height = req.query.height;
+    let weight = req.query.weight;
+
+    res.render('calc', {age: age, height: height, weight: weight});
 });
 
 app.listen(8080, () => {
